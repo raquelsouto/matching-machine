f8d1f5fa908e9c9a4128be0fbf8cb65d4f84120a
Raquel Winkeler <kelsouto@gmail.com>
2026-08-01 14:10:25 +0200

    Adjust kafka configuration

---
+++ src/test/java/com/rwinkeler/matchingengine/MatchingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwinkeler.matchingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MatchingEngineApplication.class)
+class MatchingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
